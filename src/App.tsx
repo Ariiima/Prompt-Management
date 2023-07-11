6cbad38ff335cef0efa2b2a0bdaa7ac09d425d4d
Hamidreza <kaedehara@Hamidrezas-MacBook-Air.local>
2023-07-11 21:07:22 +0330

    First commit with prompts list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PromptCard from './components/PromptCard';
 
-function App() {
+function PromptList() {
+  const prompts = [
+    { id: 1, title: 'Prompt 1', description: 'Description of Prompt 1' },
+    { id: 2, title: 'Prompt 2', description: 'Description of Prompt 2' },
+    { id: 3, title: 'Prompt 3', description: 'Description of Prompt 3' },
+    { id: 4, title: 'Prompt 4', description: 'Description of Prompt 4' },
+    { id: 5, title: 'Prompt 5', description: 'Description of Prompt 5' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center">
+      {prompts.map((prompt) => (
+        <div key={prompt.id} className="m-4">
+          <PromptCard
+            title={prompt.title}
+            description={prompt.description}
+          />
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default PromptList;
